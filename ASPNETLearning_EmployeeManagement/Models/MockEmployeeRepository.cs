c18e43a8bd8d50e66fc888df267d900c6ed152aa
Anand Neema <anand.neema14@gmail.com>
2019-06-22 19:30:33 +0530

    Added MVC to the project

---
+++ ASPNETLearning_EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETLearning_EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _empList;
+
+        public MockEmployeeRepository()
+        {
+            _empList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Anand", Department = "IT", Email = "anand.neema14@gmail.com" },
+                new Employee() { Id = 1, Name = "Anand", Department = "IT", Email = "anand.neema14@gmail.com" },
+                new Employee() { Id = 1, Name = "Anand", Department = "IT", Email = "anand.neema14@gmail.com" },
+            };
+        }
+        public Employee GetEmployee(int id)
+        {
+            return _empList.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
--- ASPNETLearning_EmployeeManagement/Startup.cs
+++ ASPNETLearning_EmployeeManagement/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNETLearning_EmployeeManagement.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,9 @@ namespace ASPNETLearning_EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +29,8 @@ namespace ASPNETLearning_EmployeeManagement
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
