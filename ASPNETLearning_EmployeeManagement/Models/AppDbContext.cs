4a5439e978c36fd2fdbde4398dad16df42fe8fa7
Anand Neema <anand.neema14@gmail.com>
2019-07-15 23:04:04 +0530

    Added DBContext Class

---
+++ ASPNETLearning_EmployeeManagement/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETLearning_EmployeeManagement.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options) //Calling base class constructor here
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
